eb4dd0d1e57eee96b511ab00e0895bead04e91ae
stiven212 <58042139+stiven212@users.noreply.github.com>
2019-12-03 14:18:57 -0500

    Add files via upload

---
+++ ordenamiento por seleccion/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+void agregar(int a[], int n);
+void imprimir(int a[], int n);
+void menor(int a[], int n);
+
+
+
+int main()
+{
+    int n;
+    cout << "Ingrese el tamaño: ";
+    cin >> n;
+    int b[n];
+    agregar(b,n);
+    imprimir(b,n);
+    menor(b,n);
+
+
+    return 0;
+}
+void agregar(int a[], int n){
+
+    for(int i=0; i<n; i++){
+        cout << "Ingrese numero "<<i+1<<": ";
+        cin>> a[i];
+    }
+
+
+
+
+}
+void imprimir(int a[], int n){
+
+    for(int i=0; i<n; i++){
+        cout<< a[i];
+    }
+}
+void menor(int a[], int n){
+
+    int b;
+    b=a[0];
+    for(int i=1; i<n; i++){
+        if(b>a[i]){
+
+            b=a[i];
+        }
+    }
+    cout <<" - Menor elemento es: " << b;
+
+}
